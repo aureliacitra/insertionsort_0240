eb53d43f5dd3d4a4e846199685f35ce972c23e9f
aureliacitra <citrapangukir@gmail.com>
2025-03-18 21:59:59 +0700

    add if statement

--- ContohInsertinSort.cpp
+++ ContohInsertinSort.cpp
@@ -10,3 +10,16 @@ void input()
 {
 cout << "Masuk Jumlah Data pada Array : "; // Membuat Input jumlah element Array
 cin >> n; // memanggil variable input n
+
+if (n <= 20)
+{ // Membuat Kondisi n tidak lebih dari 20
+    break;
+}
+else{
+    cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; // Menampilkan pesan jika data lebih dari 20
+}
+}
+cout <<  endl; // Membuat jarak per baris program
+cout << "======================" << endl; // Membuat tampilan susunan data element array
+cout << "Masukkan Element Array" << endl;
+cout << "======================" << endl;
